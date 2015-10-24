7df909488dbbf147b3c9d1e2081458246f795937
blundell <paul@novoda.com>
2015-10-24 20:35:40 +0100

    removes repeats so we do less work (less frame skipping)

--- mobile/src/androidTest/java/com/novoda/canvas/TheMatrixActivityTest.java
+++ mobile/src/androidTest/java/com/novoda/canvas/TheMatrixActivityTest.java
@@ -13,77 +13,60 @@ import java.util.Random;
 public class TheMatrixActivityTest extends NovodaActivityTest {
 
     private static final int FONT_SIZE = 20;
-    private static final int NUMBER_OF_REPEATS = 5;
-    private static final String TEXT = "セ ソ キ ク ケ コ イ ウ エ オ ジャ な あ た ア カ サ ザ ジ  ズ ゼ ゾ シ ス";
+    private static final String CHARACTERS = "セ ソ キ ク ケ コ イ ウ エ オ ジャ な あ た ア カ サ ザ ジ  ズ ゼ ゾ シ ス";
     private static final Random random = new Random();
 
-    private final TextView[] rain = new TextView[NUMBER_OF_REPEATS];
-
     private ViewGroup parent;
+    private int randomPositionX;
+    private TextView view;
 
     @Override
     public void startTestFor(Activity activity) {
         parent = getParent(activity);
         parent.setBackgroundColor(getColor(R.color.matrix_background));
-
-        final int[] randomPositionX = new int[NUMBER_OF_REPEATS];
-        scroll(randomPositionX);
-
+        createRowOfRainDropViews();
+        scroll();
     }
 
-    private void scroll(int[] randomPositionX) {
-        parent.postDelayed(scrollMatrix(randomPositionX), 200);
+    private void scroll() {
+        parent.postDelayed(scrollMatrix(), 200);
     }
 
-    private Runnable scrollMatrix(final int[] randomPositionX) {
+    private Runnable scrollMatrix() {
         return new Runnable() {
             @Override
             public void run() {
-                draw(randomPositionX);
-                scroll(randomPositionX);
+                draw();
+                scroll();
             }
         };
     }
 
-    private void draw(int[] randomPositionX) {
-        selectRandomCharacters(randomPositionX);
-
-        for (int j = 0; j < (parent.getHeight() / FONT_SIZE) * 2; j++) {
+    private void draw() {
+        for (int y = 0; y < (parent.getHeight() / FONT_SIZE) * 2; y++) {
             char character = getRandomCharacter();
 
-            for (int i = 0; i < NUMBER_OF_REPEATS; i++) {
-                TextView charView = rain[i];
-                charView.setX(randomPositionX[i] * FONT_SIZE);
-                charView.setY(j * (FONT_SIZE / 2));
-
-                charView.setText(String.valueOf(character));
-                charView.setTextColor(Color.rgb(0, 255, 0));
-                selectRandomCharacters(randomPositionX);
-            }
+            TextView charView = view;
+            charView.setX(randomPositionX);
+            charView.setY(y * (FONT_SIZE / 2));
+            charView.setText(String.valueOf(character));
+            charView.setTextColor(Color.rgb(0, 255 - y, 0));
+            createRowOfRainDropViews();
         }
     }
 
-    private void selectRandomCharacters(int[] randomPositionX) {
-        for (int i = 0; i < NUMBER_OF_REPEATS; i++) {
-            char character = getRandomCharacter();
-            randomPositionX[i] = random.nextInt(parent.getWidth() / FONT_SIZE) - 1;
-
-            TextView label = createTextView();
-            rain[i] = label;
-            label.setText(String.valueOf(character));
-            int color = Color.rgb(0, 255 - (i * 5), 0);
-            label.setTextColor(color);
-            parent.addView(label);
-            if (Color.green(color) <= 250) {
-                parent.removeView(rain[i]);
-//                i = (parent.getHeight() / FONT_SIZE) * 2;
-            }
-        }
+    private void createRowOfRainDropViews() {
+        TextView view = createTextView();
+        char character = getRandomCharacter();
+        view.setText(String.valueOf(character));
+        parent.addView(view);
+        this.view = view;
+        randomPositionX = (random.nextInt(parent.getWidth() / FONT_SIZE) - 1) * FONT_SIZE;
     }
 
     private char getRandomCharacter() {
-        int randomCharPosition = random.nextInt(TEXT.length());
-        return TEXT.charAt(randomCharPosition);
+        int randomCharPosition = random.nextInt(CHARACTERS.length());
+        return CHARACTERS.charAt(randomCharPosition);
     }
 
     private TextView createTextView() {
