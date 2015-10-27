c01889d9abf5e7a1e0ffe77b812043e318c386be
Daniele Bonaldo <dany.bony@gmail.com>
2015-10-27 15:13:18 +0000

    Add TileType to handle different tiles position logic

--- mobile/src/androidTest/java/com/novoda/canvas/TilesSpiralActivityTest.java
+++ mobile/src/androidTest/java/com/novoda/canvas/TilesSpiralActivityTest.java
@@ -16,7 +16,7 @@ import static com.novoda.canvas.NovodaActivity.RANDOM;
 
 public class TilesSpiralActivityTest extends NovodaActivityTest {
 
-    private static final int TILES_COUNT = 10;
+    private static final int TILES_COUNT = 30;
     private static final int MIN_SIZE = 100;
     private static final int FADE_DURATION = 1000;
 
@@ -27,11 +27,13 @@ public class TilesSpiralActivityTest extends NovodaActivityTest {
     public void startTestFor(Activity activity) {
         ViewGroup parent = getParent(activity);
         availableRect = new Rect(parent.getLeft(), parent.getTop(), parent.getRight(), parent.getBottom());
+        TileType type = TileType.BOTTOM;
 
         for (int i = 0; i < TILES_COUNT && screenNotFullAlready(); i++) {
-            View tile = createTile(activity);
+            View tile = createTile(type, activity);
             parent.addView(tile);
             revealTile(tile);
+            type = type.getNext();
         }
 
     }
@@ -40,53 +42,128 @@ public class TilesSpiralActivityTest extends NovodaActivityTest {
         return availableRect.width() > 0 && availableRect.height() > 0;
     }
 
-    private View createTile(Context context) {
+    private View createTile(TileType type, Context context) {
         int color = randomColorFactory.getColor();
 
         GradientDrawable drawable = new GradientDrawable();
         drawable.setShape(GradientDrawable.RECTANGLE);
         drawable.setColor(color);
 
-        int tileTop = availableRect.top;
-        int tileLeft = availableRect.left;
-        int tileWidth = getTileWidth();
-        int tileHeight = availableRect.height();
+        int tileTop = type.getTop(availableRect);
+        int tileLeft = type.getLeft(availableRect);
+        int tileRight = type.getRight(availableRect);
+        int tileBottom = type.getBottom(availableRect);
 
         View square = new View(context);
         square.setBackground(drawable);
-        square.setLayoutParams(new LinearLayout.LayoutParams(tileWidth, tileHeight));
+        square.setLayoutParams(
+                new LinearLayout.LayoutParams(
+                        tileRight - tileLeft,
+                        tileBottom - tileTop
+                )
+        );
 
         square.setY(tileTop);
         square.setX(tileLeft);
 
-        availableRect.set(tileLeft + tileWidth, availableRect.top, availableRect.right, availableRect.bottom);
+        type.updateAvailableSpace(availableRect, tileLeft, tileTop, tileRight, tileBottom);
 
         return square;
-    }
-
-    private int getTileWidth() {
-        return Math.min(
-                availableRect.width(),
-                Math.max(
-                        MIN_SIZE,
-                        RANDOM.nextInt(availableRect.width() / 2)
-                )
-        );
-    }
-
-    private int getTileHeight() {
-        return Math.min(
-                availableRect.height(),
-                Math.max(
-                        MIN_SIZE,
-                        RANDOM.nextInt(availableRect.height() / 2)
-                )
-        );
     }
 
     private void revealTile(View square) {
         ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(square, "alpha", 0f, 1f);
         alphaAnimator.setDuration(FADE_DURATION);
         alphaAnimator.start();
+    }
+
+    private enum TileType {
+        BOTTOM {
+            @Override
+            public int getTop(Rect availableRect) {
+                return availableRect.bottom - getRandomHeight(availableRect);
+            }
+
+            @Override
+            public void updateAvailableSpace(Rect availableRect, int tileLeft, int tileTop, int tileRight, int tileBottom) {
+                availableRect.set(availableRect.left, availableRect.top, availableRect.right, tileTop);
+            }
+        },
+        RIGHT {
+            @Override
+            public int getLeft(Rect availableRect) {
+                return availableRect.right - getRandomWidth(availableRect);
+            }
+
+            @Override
+            public void updateAvailableSpace(Rect availableRect, int tileLeft, int tileTop, int tileRight, int tileBottom) {
+                availableRect.set(availableRect.left, availableRect.top, tileLeft, availableRect.bottom);
+            }
+        },
+        TOP {
+            @Override
+            public int getBottom(Rect availableRect) {
+                return availableRect.top + getRandomHeight(availableRect);
+            }
+
+            @Override
+            public void updateAvailableSpace(Rect availableRect, int tileLeft, int tileTop, int tileRight, int tileBottom) {
+                availableRect.set(availableRect.left, tileBottom, availableRect.right, availableRect.bottom);
+            }
+        },
+        LEFT {
+            @Override
+            public int getRight(Rect availableRect) {
+                return availableRect.left + getRandomWidth(availableRect);
+            }
+
+            @Override
+            public void updateAvailableSpace(Rect availableRect, int tileLeft, int tileTop, int tileRight, int tileBottom) {
+                availableRect.set(tileRight, availableRect.top, availableRect.right, availableRect.bottom);
+            }
+        };
+
+        TileType getNext() {
+            int nextIndex = (this.ordinal() + 1) % TileType.values().length;
+            return TileType.values()[(nextIndex)];
+        }
+
+        public int getTop(Rect availableRect) {
+            return availableRect.top;
+        }
+
+        public int getLeft(Rect availableRect) {
+            return availableRect.left;
+        }
+
+        public int getRight(Rect availableRect) {
+            return availableRect.right;
+        }
+
+        public int getBottom(Rect availableRect) {
+            return availableRect.bottom;
+        }
+
+        private static int getRandomWidth(Rect availableRect) {
+            return Math.min(
+                    availableRect.width(),
+                    Math.max(
+                            MIN_SIZE,
+                            RANDOM.nextInt(availableRect.width() / 2)
+                    )
+            );
+        }
+
+        private static int getRandomHeight(Rect availableRect) {
+            return Math.min(
+                    availableRect.height(),
+                    Math.max(
+                            MIN_SIZE,
+                            RANDOM.nextInt(availableRect.height() / 2)
+                    )
+            );
+        }
+
+        public abstract void updateAvailableSpace(Rect availableRect, int tileLeft, int tileTop, int tileRight, int tileBottom);
     }
 }
