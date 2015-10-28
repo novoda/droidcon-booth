61cd134ed636b35ba08f9912024b142867c4b54c
grujo <zvonko@novoda.com>
2015-10-28 15:55:52 +0000

    Add name for each city

--- mobile/src/androidTest/java/com/novoda/canvas/CitiesTest.java
+++ mobile/src/androidTest/java/com/novoda/canvas/CitiesTest.java
@@ -1,9 +1,13 @@
 package com.novoda.canvas;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.novoda.canvas.base.NovodaActivityTest;
 
@@ -17,6 +21,14 @@ public class CitiesTest extends NovodaActivityTest {
             R.drawable.barcelona,
             R.drawable.liverpool,
             R.drawable.new_york
+    };
+
+    private static String[] CITY_NAMES = {
+            "LONDON",
+            "BERLIN",
+            "BARCELONA",
+            "LIVERPOOL",
+            "NEW YORK"
     };
 
     @Override
@@ -34,16 +46,26 @@ public class CitiesTest extends NovodaActivityTest {
         cityImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         root.addView(cityImageView);
 
+        final TextView cityTextView = new TextView(activity);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        params.setMargins(0, 20, 0, 20);
+        cityTextView.setLayoutParams(params);
+        cityTextView.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.TOP);
+        cityTextView.setTextColor(Color.WHITE);
+        cityTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 48);
+        root.addView(cityTextView);
+
         for (int i = 0; i < CITIES.length; i++) {
             final int nextCity = CITIES[i];
+            final String nextCityName = CITY_NAMES[i];
             root.postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     cityImageView.setImageResource(nextCity);
+                    cityTextView.setText(nextCityName);
                 }
             }, i * CITY_CHANGE_INTERVAL);
         }
     }
-
 
 }
