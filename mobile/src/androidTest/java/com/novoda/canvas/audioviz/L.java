146523f2dcd052ee4a711d9b080d3107c3ef1de0
takecare <rui@vazteixeira.org>
2015-10-28 23:27:27 +0000

    adding logging util class

---
+++ mobile/src/androidTest/java/com/novoda/canvas/audioviz/L.java
@@ -0,0 +1,28 @@
+package com.novoda.canvas.audioviz;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+public class L {
+
+    public static void d(String t, String m) {
+        Log.d(t, m);
+    }
+
+    public static void d(Object o, Object m) {
+        d(o.getClass().getSimpleName(), m.toString());
+    }
+
+    public static void t(final Object o, final Object m) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        d(o, m);
+                    }
+                }
+        );
+    }
+}
