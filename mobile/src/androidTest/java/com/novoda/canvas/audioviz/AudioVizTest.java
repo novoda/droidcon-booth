8d4dfd31669008d74980436934f4f420b21e0bef
takecare <rui@vazteixeira.org>
2015-10-28 23:14:29 +0000

    adding empty test

---
+++ mobile/src/androidTest/java/com/novoda/canvas/audioviz/AudioVizTest.java
@@ -0,0 +1,24 @@
+package com.novoda.canvas.audioviz;
+
+import android.app.Activity;
+import android.view.ViewGroup;
+
+import com.novoda.canvas.base.NovodaActivityTest;
+
+import org.junit.After;
+
+public class AudioVizTest extends NovodaActivityTest {
+
+    @Override
+    public void startTestFor(Activity activity) {
+
+        final ViewGroup parent = getParent();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
