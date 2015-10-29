4c7123c19883c9844e3ee049a7cec333918cb348
takecare <rui@vazteixeira.org>
2015-10-29 00:39:27 +0000

    refactoring view: reducing visibility

--- mobile/src/androidTest/java/com/novoda/canvas/audioviz/AmplitudeView.java
+++ mobile/src/androidTest/java/com/novoda/canvas/audioviz/AmplitudeView.java
@@ -7,25 +7,25 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.view.View;
 
-public class AmplitudeView extends View implements Tickable {
+class AmplitudeView extends View implements Tickable {
 
-    Paint bgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    Paint fgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    Path path = new Path();
+    private Paint backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint foregroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Path path = new Path();
 
-    SoundDataProvider soundDataProvider;
+    private SoundDataProvider soundDataProvider;
 
     public AmplitudeView(Context context, SoundDataProvider soundDataProvider) {
         super(context);
-        bgPaint.setColor(Color.RED);
-        fgPaint.setColor(Color.GREEN);
+        backgroundPaint.setColor(Color.RED);
+        foregroundPaint.setColor(Color.GREEN);
         this.soundDataProvider = soundDataProvider;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
-        canvas.drawPaint(bgPaint);
+        canvas.drawPaint(backgroundPaint);
 
         int width = canvas.getWidth();
         int height = canvas.getHeight();
@@ -39,7 +39,7 @@ public class AmplitudeView extends View implements Tickable {
             path.lineTo(width / 5, amplitude);
             path.close();
 
-            canvas.drawPath(path, fgPaint);
+            canvas.drawPath(path, foregroundPaint);
         }
 
         super.onDraw(canvas);
