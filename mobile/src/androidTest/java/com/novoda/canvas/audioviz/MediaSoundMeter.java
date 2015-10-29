808aed3ed9badc733808c780e27d2f2857f4c03c
takecare <rui@vazteixeira.org>
2015-10-29 11:18:55 +0000

    adding media recorder based sound meter

---
+++ mobile/src/androidTest/java/com/novoda/canvas/audioviz/MediaSoundMeter.java
@@ -0,0 +1,58 @@
+package com.novoda.canvas.audioviz;
+
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
+public class MediaSoundMeter implements SoundDataRetriever, SoundDataProvider, Tickable {
+
+    private MediaRecorder mediaRecorder = null;
+    private int lastSampledAmplitude;
+
+    @Override
+    public int getAmplitude() {
+        return lastSampledAmplitude;
+    }
+
+    @Override
+    public int getMean() {
+        return 0;
+    }
+
+    @Override
+    public void start() {
+        if (mediaRecorder == null) {
+            mediaRecorder = new MediaRecorder();
+            try {
+                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+                mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+                mediaRecorder.setOutputFile("/dev/null");
+                mediaRecorder.prepare();
+                mediaRecorder.start();
+            } catch (IllegalStateException | IOException e) {
+                L.d(this, "start() failed: " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (mediaRecorder != null) {
+            mediaRecorder.stop();
+            mediaRecorder.reset();
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
+    }
+
+    @Override
+    public void read() {
+        lastSampledAmplitude = (int) (mediaRecorder.getMaxAmplitude() / 2700.0);
+    }
+
+    @Override
+    public void tick() {
+        read();
+    }
+}
