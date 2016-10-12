8253559b0ddf361e8067acc7f5caad5651959f90
Adam Brown <adam@novoda.com>
2016-10-12 19:47:24 +0100

    setting infinite running script to use this branch

--- infinite-run.sh
+++ infinite-run.sh
@@ -2,7 +2,7 @@
 
 while true; do
 
- git pull origin REPLACE_ME
+ git pull origin droidcon-2016
 
  /opt/android-sdk/platform-tools/adb kill-server
  /opt/android-sdk/platform-tools/adb start-server
