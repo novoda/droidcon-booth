5c4b7affa101202fd423509f65c59b6bd5338564
Sebastiano Poggi <poggos@gmail.com>
2016-10-26 13:30:05 +0100

    Improve base class

--- mobile/src/androidTest/java/com/novoda/canvas/base/NovodaActivityTest.java
+++ mobile/src/androidTest/java/com/novoda/canvas/base/NovodaActivityTest.java
@@ -1,39 +1,40 @@
 package com.novoda.canvas.base;
 
 import android.app.Activity;
-import android.os.Build;
 import android.os.SystemClock;
 import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.ViewGroup;
 
 import com.novoda.canvas.NovodaActivity;
-
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeUnit;
+
 @RunWith(AndroidJUnit4.class)
 public abstract class NovodaActivityTest {
 
-    public static final int TIME_LIMIT_FOR_TEST_IN_SECONDS = 10;
+    protected static final int TIME_LIMIT_FOR_TEST_IN_SECONDS = 10;
+
     @Rule
     public ActivityTestRule<NovodaActivity> activityRule = new ActivityTestRule<>(NovodaActivity.class);
 
     private NovodaActivity activity;
 
     @Before
-    public void setUp() {
+    public final void setUp() {
         activity = activityRule.getActivity();
     }
 
     @Test
-    public void theTest() throws InterruptedException {
+    public final void theTest() throws InterruptedException {
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -51,11 +52,7 @@ public abstract class NovodaActivityTest {
 
     @ColorInt
     protected int getColor(@ColorRes int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return activity.getResources().getColor(color, activity.getTheme());
-        } else {
-            return activity.getResources().getColor(color);
-        }
+        return ResourcesCompat.getColor(activity.getResources(), color, activity.getTheme());
     }
 
 }
