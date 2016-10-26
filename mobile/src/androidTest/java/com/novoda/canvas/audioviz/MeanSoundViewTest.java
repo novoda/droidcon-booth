7b867936be076979ed6b45fbb268e3ae00cd1296
Sebastiano Poggi <poggos@gmail.com>
2016-10-26 14:32:15 +0100

    Request recording permission for sound meter

--- mobile/src/androidTest/java/com/novoda/canvas/audioviz/MeanSoundViewTest.java
+++ mobile/src/androidTest/java/com/novoda/canvas/audioviz/MeanSoundViewTest.java
@@ -1,41 +1,53 @@
 package com.novoda.canvas.audioviz;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.view.ViewGroup;
 
 import com.novoda.canvas.base.NovodaActivityTest;
 
+import org.junit.After;
+
 public class MeanSoundViewTest extends NovodaActivityTest {
 
     private SimpleSoundMeter simpleSoundMeter;
-    private SoundDataRetriever soundDataRetriever;
-    private SoundDataProvider soundDataProvider;
-
     private Ticker ticker;
 
     @Override
     public void startTestFor(Activity activity) {
+        ensureAudioRecordPermission();
+
         setupDependencies();
 
         ViewGroup parent = getParent();
         Context context = parent.getContext();
 
-        MeanSoundView meanSoundView = new MeanSoundView(context, soundDataProvider);
+        MeanSoundView meanSoundView = new MeanSoundView(context, simpleSoundMeter);
         parent.addView(meanSoundView);
 
         ticker.addTickable(simpleSoundMeter);
         ticker.addTickable(meanSoundView);
     }
 
+    private void ensureAudioRecordPermission() {
+        ensurePermissions(
+                new String[] {Manifest.permission.RECORD_AUDIO},
+                "Need audio recording permission to show audio meter"
+        );
+    }
+
     public void setupDependencies() {
         simpleSoundMeter = new SimpleSoundMeter();
-        soundDataRetriever = simpleSoundMeter;
-        soundDataProvider = simpleSoundMeter;
         ticker = new Ticker.TickerImpl();
 
-        soundDataRetriever.start();
+        simpleSoundMeter.start();
         ticker.start();
     }
 
+    @After
+    public void tearDown() {
+        ticker.stop();
+        simpleSoundMeter.stop();
+    }
 }
